31e70fdc529be3a5786733846c06b3dea912619b
Robert Spica <rspica@Roberts-iMac.local>
2017-05-03 07:44:26 -0400

    Added set attribute to all crystals for values, added eventListener main.js, HTML updates add required.

--- assets/crystal-main.js
+++ assets/crystal-main.js
@@ -1,8 +1,8 @@
 // user experience
 // =================================================
-// player starts game to display a random number .
-// player selects one four crystals displayed.
-// Each crystal has an assigned value not known to the player.
+// xxx player starts game to display a random number .
+// xxx player selects one four crystals displayed.
+// xxx Each crystal has an assigned value not known to the player.
 // player trys to select the right amount of crystal to match the random number total displayed
 // games shows and tracks total score after each cystal is clicked
 // game displays number of wins and losses
@@ -30,8 +30,6 @@
 //-------------  ** Global Variables ** -------------
 //---------------------------------------------------
 
-// Array to store random number for each of 4 crystals
-var crystalValue = [];
 
 // win count variable
 var winCounter = 0;
@@ -40,12 +38,14 @@ var winCounter = 0;
 var lossCounter = 0;
 
 // random number for crystal to total to
-var numberGenerate = 0;
+var KeyValueCompare;
 
 // total crystal value from click event score
 var CrystalTotal = 0;
 
+// Array to store random number for each of 4 crystals
 var crystalValue = [];
+
 
 // minimum number value for random number generator
 var min = 0;
@@ -58,7 +58,7 @@ var max = 0;
 //----------------  **Get Variables**  ------------------
 //-------------------------------------------------------
 //grabs crystal to set random valuesval
-	var crystalValue = document.querySelector(".crystalValue");
+//	var crystalValueAdd = document.querySelector(".crystal-[i]");
 
 
 
@@ -71,29 +71,54 @@ var max = 0;
 
 function generateRandomNumber(min, max) {
 	var numberGenerate = Math.floor(Math.random() * (max - min + 1)) + min;
-console.log(min);
-console.log(max);
+console.log("min= " + min);
+console.log("max= " + max);
 console.log(numberGenerate);
+	return numberGenerate;
 
 	}
 
-generateRandomNumber(19,120);
+KeyValueCompare = generateRandomNumber(19,120);
 
 
-// generate random numbera 4x one for each cystal values: randomize between 1 and 12
-function cystalValueAssign(min, max) {
-// generate random number (1 to 12) one for each cystal values
+// generate random number 4x one for each cystal values
+function cystalValueAssign() {
+// generate random number (randomize between 1 and 12) one for each cystal values
 	var min = 1;
-	var max = 12
-	generateRandomNumber(min, max);
-	console.log(generateRandomNumber);
-	for(i = 0; i < crystalValue.length; i++) {
-		crystalValue[i].addEventListener("click", function(event) {
-	console.log(event);	
-});
-
+	var max = 12;
+	for(i = 0; i < 4; i++) {
+		var randomNum = generateRandomNumber(min, max);
+		crystalValue.push(randomNum);
+		valueAdd(randomNum, i);
 	}
-
 }
 cystalValueAssign();
+
+// function value add assigns the random numbers to each of the four crystal
+function valueAdd(randomNum, i) {
+	console.log("going around " + i + " " + randomNum);
+	console.log(document.getElementsByClassName("crystalValue")[i]);
+	var crystalValueAdd = document.getElementsByClassName("crystalValue")[i];
+	crystalValueAdd.setAttribute("Data-crystal", randomNum);
+}
+
+	document.addEventListener("click", function(event) {
+		console.log(event);	
+		var crystalTotalValue = document.querySelector(".crystalValue");
+		console.log(crystalTotalValue);	
+		crystalTotalValue.getAttribute("data-crystal")
+		console.log(crystalTotalValue.getAttribute("data-crystal"));
+	});
+
+
+	var theParent = document.querySelector("#theDude");
+theParent.addEventListener("click", doSomething, false);
+ 
+function doSomething(e) {
+    if (e.target !== e.currentTarget) {
+        var clickedItem = e.target.id;
+        alert("Hello " + clickedItem);
+    }
+    e.stopPropagation();
+}
 
